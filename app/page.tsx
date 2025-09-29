b5abf483c06354ad9286e0cb4deb46a519b5c43c
Nhat Minh <minhntn.124010122008@vtc.edu.vn>
2025-09-29 21:19:04 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,8 @@ import InvoiceStatus from "./ui/invoices/status";
 import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
-export interface Props {
-  myName: string;
-}
-export default function Page({ myName = "John Doe" }: Props) {
+
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -19,7 +17,7 @@ export default function Page({ myName = "John Doe" }: Props) {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong className={lusitana.className}>My name is: {myName}</strong>
+            <strong className={lusitana.className}>Hello World.</strong>
           </p>
           <InvoiceStatus status="pending" />
           <Link
